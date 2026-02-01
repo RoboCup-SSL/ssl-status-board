b47cdb8c68f06ec22b8d2d3c3a3985214e8bba31
Nicolai Ommer <nicolai.ommer@gmail.com>
2026-02-01 20:53:24 +0100

    refactor: Add more game event samples to default state for testing

--- frontend/src/stores/referee.ts
+++ frontend/src/stores/referee.ts
@@ -76,9 +76,125 @@ export const useRefereeStore = defineStore('referee', () => {
       stageTimeLeft: 140000000n,
       gameEvents: [
         {
+          type: GameEvent_Type.GOAL,
+          origin: [ORIGIN_TIGERS],
+          createdTimestamp: 100000000n,
+          event: {
+            case: 'goal',
+            value: {
+              byTeam: Team.YELLOW,
+              kickingBot: 5,
+              kickingTeam: Team.YELLOW,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.BOT_CRASH_UNIQUE,
+          origin: [ORIGIN_ERFORCE],
+          createdTimestamp: 200000000n,
+          event: {
+            case: 'botCrashUnique',
+            value: {
+              byTeam: Team.BLUE,
+              violator: 3,
+              victim: 7,
+              crashSpeed: 5.2,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.BOT_KICKED_BALL_TOO_FAST,
+          origin: [ORIGIN_TIGERS, ORIGIN_ERFORCE],
+          createdTimestamp: 300000000n,
+          event: {
+            case: 'botKickedBallTooFast',
+            value: {
+              byTeam: Team.YELLOW,
+              byBot: 2,
+              initialBallSpeed: 7.8,
+              chipped: false,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.DEFENDER_IN_DEFENSE_AREA,
+          origin: [ORIGIN_TIGERS],
+          createdTimestamp: 400000000n,
+          event: {
+            case: 'defenderInDefenseArea',
+            value: {
+              byTeam: Team.BLUE,
+              byBot: 0,
+              distance: 0.15,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.UNSPORTING_BEHAVIOR_MINOR,
+          origin: [ORIGIN_ERFORCE],
+          createdTimestamp: 500000000n,
+          event: {
+            case: 'unsportingBehaviorMinor',
+            value: {
+              byTeam: Team.YELLOW,
+              reason: 'Excessive celebration',
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.TOO_MANY_ROBOTS,
+          origin: [ORIGIN_TIGERS, ORIGIN_ERFORCE],
+          createdTimestamp: 600000000n,
+          event: {
+            case: 'tooManyRobots',
+            value: {
+              byTeam: Team.BLUE,
+              numRobotsAllowed: 11,
+              numRobotsOnField: 12,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.PLACEMENT_SUCCEEDED,
+          origin: [ORIGIN_TIGERS],
+          createdTimestamp: 700000000n,
+          event: {
+            case: 'placementSucceeded',
+            value: {
+              byTeam: Team.YELLOW,
+              timeTaken: 3.5,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.BOT_CRASH_DRAWN,
+          origin: [ORIGIN_ERFORCE],
+          createdTimestamp: 800000000n,
+          event: {
+            case: 'botCrashDrawn',
+            value: {
+              botYellow: 6,
+              botBlue: 8,
+              crashSpeed: 2.1,
+            },
+          },
+        },
+        {
+          type: GameEvent_Type.PENALTY_KICK_FAILED,
+          origin: [ORIGIN_TIGERS, ORIGIN_ERFORCE],
+          createdTimestamp: 900000000n,
+          event: {
+            case: 'penaltyKickFailed',
+            value: {
+              byTeam: Team.BLUE,
+              reason: 'Keeper stopped the ball',
+            },
+          },
+        },
+        {
           type: GameEvent_Type.BALL_LEFT_FIELD_GOAL_LINE,
           origin: [ORIGIN_TIGERS, ORIGIN_ERFORCE],
-          createdTimestamp: 100000000n,
+          createdTimestamp: 1000000000n,
           event: {
             case: 'ballLeftFieldGoalLine',
             value: {
